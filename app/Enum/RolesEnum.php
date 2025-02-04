d0f91f38922e3fc4841306ca552f1a82758e46cb
Austin <amazingaj73@yahoo.com>
2025-02-04 01:41:30 -0500

    some permissions and role stuff

---
+++ app/Enum/RolesEnum.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Enum;
+
+enum RolesEnum: string
+{
+    case Admin = 'admin';
+    case Commenter = 'commenter';
+    case User = 'user';
+    
+    public static function labels(): array
+    {
+        return [
+            self::Admin->value => 'Admin',
+            self::Commenter->value => 'Commenter',
+            self::User->value => 'User'
+        ];
+    }
+    public function label()
+    {
+        return match($this){
+            self::Admin => 'Admin',
+            self::Commenter => 'Commenter',
+            self::User => 'User',
+        };
+    }
+}
